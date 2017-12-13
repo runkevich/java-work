b462141ba07759c31550347582583959640d1d48
Ekaterina Runkevich <runkevich8@gmail.com>
2017-12-13 19:46:18 +0300

    arrayList

---
+++ Homework/homework9/src/com/gmail/runkevich8/Task1/Main.java
@@ -0,0 +1,41 @@
+package com.gmail.runkevich8.Task1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<String> array = new ArrayList<>();
+
+        boolean i = true;
+        while (true){
+
+            System.out.println("Вводите любые данные:");
+
+            Scanner in = new Scanner(System.in);
+            String line = in.nextLine();
+            if (line.equals("stop")){
+                System.out.println(array);
+                fix(array);
+
+                return;
+            }
+            array.add(line);
+        }
+
+    }
+
+    private static List<String> fix(List<String> array) {
+
+        for (int i = 0; i<=array.size()-1; i++){
+
+           System.out.println( array.get(i).replace("a",""));
+        }
+
+        return array;
+    }
+
+}
