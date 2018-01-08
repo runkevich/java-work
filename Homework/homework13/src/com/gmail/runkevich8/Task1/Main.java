7f4f4f6301bac7ca784e8a833c5a5628c0d96d54
Ekaterina Runkevich <runkevich8@gmail.com>
2018-01-08 10:43:05 +0300

    doneT3

--- Homework/homework13/src/com/gmail/runkevich8/Task1/Main.java
+++ Homework/homework13/src/com/gmail/runkevich8/Task1/Main.java
@@ -1,6 +1,9 @@
 package com.gmail.runkevich8.Task1;
 
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
 
@@ -13,6 +16,17 @@ public class Main {
         text = text.replaceAll("\\s","");
         System.out.println(text);
 
+        Pattern pattern = Pattern.compile("((.)+)\\1+");
+        Matcher matcher = pattern.matcher(text);
+        StringBuffer buffer = new StringBuffer();
+        while (matcher.find()) {
+
+            int cur = matcher.end() - matcher.start();
+            String w = matcher.group().substring(0, 1);
+            matcher.appendReplacement(buffer, w + cur);
+        }
+        matcher.appendTail(buffer);
+        System.out.println(buffer.toString());
 
     }
 
