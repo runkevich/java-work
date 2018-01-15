88abf8e6da432ee4ca4a7bf6f90cc251f2d10716
Ekaterina Runkevich <runkevich8@gmail.com>
2018-01-15 23:33:36 +0300

    fixed

--- Homework/homework15/src/com/gmail/runkevich8/Task1/Main.java
+++ Homework/homework15/src/com/gmail/runkevich8/Task1/Main.java
@@ -1,30 +1,45 @@
 package com.gmail.runkevich8.Task1;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
     public static void main(String[] args) {
-
         Main main = new Main();
-        Thread thread1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i<10; i++){
-                    main.print10();
-                }
-            }
-        });
-        Thread thread2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i = 0; i< 10; i++){
-                 main.print10();
-                }
-            }
-        });
+        System.out.println("Введите start для старта");
+        Scanner in = new Scanner(System.in);
+        String start = in.nextLine();
 
-        thread1.start();
-        thread2.start();
+        Pattern pattern = Pattern.compile("^start$");
+        Matcher matcher = pattern.matcher(start);
 
+
+        if (matcher.matches()) {
+            Thread thread1 = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int i = 0; i < 10; i++) {
+                        main.print10();
+                    }
+                }
+            });
+            Thread thread2 = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int i = 0; i < 10; i++) {
+                        main.print10();
+                    }
+                }
+            });
+
+            thread1.start();
+            thread2.start();
+
+        } else {
+            System.out.println("Не то значение ввели для старта");
+        }
     }
 
     public synchronized void print10(){
